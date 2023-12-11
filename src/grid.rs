b745c811e45d627add1bfe43fdcd9309aabe7283
Yjn024 <jiening.yu@outlook.com>
2023-12-11 09:28:04 +0800

    based

--- src/grid.rs
+++ src/grid.rs
@@ -13,7 +13,33 @@ pub struct Grid {
     tiles: Vec<Vec<Tile>>,
 }
 
+#[derive(Debug)]
+pub struct ConflictDescriptor<'a> {
+    pub locs: &'a [Loc],
+
+    pub available_locs_num: usize,
+    /// Number of starting locations.
+    ///
+    /// Can be 2, 3, or 4.
+    pub locs_num: usize,
+
+    /// Ids of the possible opponents.
+    pub players: &'a [u32],
+    pub ui_players: &'a [u32],
+
+    /// 1, ... number of available locations.
+    /// 1 is the best.
+    pub conditions: usize,
+    /// Inequality from 0 to 4.
+    pub ineq: u32,
+}
+
 impl Grid {
+    /// Creates a new grid with given width and height.
+    /// Tiles are generated randomly, including mountains,
+    /// mines and cities.
+    ///
+    /// See [`Tile::new`].
     pub fn new(width: u32, height: u32) -> Self {
         Self {
             width: width.min(crate::MAX_WIDTH),
@@ -24,10 +50,49 @@ impl Grid {
                 .map(|_| {
                     vec![(); height as usize]
                         .into_iter()
-                        .map(|_| Tile::rand())
+                        .map(|_| Tile::new())
                         .collect()
                 })
                 .collect(),
+        }
+    }
+
+    pub fn conflict(&mut self, descriptor: ConflictDescriptor<'_>) {
+        let ConflictDescriptor {
+            locs,
+            available_locs_num,
+            locs_num,
+            players,
+            ui_players,
+            conditions,
+            ineq,
+        } = descriptor;
+
+        // Remove all cities.
+        for arr in self.tiles.iter_mut() {
+            for tile in arr {
+                if let Tile::Habitable { land, units, owner } = tile {
+                    units.copy_from_slice(&[0; 8]);
+                    *owner = Player::NEUTRAL;
+                    *land = HabitLand::Grassland;
+                }
+            }
+        }
+
+        let locs_num = in_segment!(locs_num, 2, available_locs_num);
+        let num = locs_num.min(players.len() + ui_players.len());
+        let di = fastrand::usize(..available_locs_num);
+
+        let mut chosen_locs = vec![Loc(0, 0); num];
+        for (i, loc) in chosen_locs.iter_mut().enumerate() {
+            let ii = (i + di + available_locs_num) % available_locs_num;
+            *loc = locs[ii];
+            let Loc(x, y) = *loc;
+            self.tiles[x as usize][y as usize].set_habitable(HabitLand::Fortress);
+
+            // Place mines nearby
+            let Loc(ri, rj) = fastrand::choice(Loc::DIRS).unwrap();
+            self.tiles[(x + ri) as usize][(y + rj) as usize] = Tile::Mine(Player::NEUTRAL);
         }
     }
 }
@@ -51,6 +116,13 @@ impl Loc {
         Self(1, -1),
         Self(-1, 1),
     ];
+}
+
+impl From<(u32, u32)> for Loc {
+    #[inline]
+    fn from((x, y): (u32, u32)) -> Self {
+        Self(x as i32, y as i32)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -89,13 +161,13 @@ impl Tile {
         }
     }
 
-    /// Randomly generate a tile from scratch.
-    fn rand() -> Self {
+    /// Randomly generates a tile from scratch.
+    fn new() -> Self {
         let mut this = Self::default();
-        match fastrand::u32(..) % 20 {
+        match fastrand::u32(..20) {
             0 => {
                 this = Tile::Habitable {
-                    land: match fastrand::u32(..) % 6 {
+                    land: match fastrand::u32(..6) {
                         0 => HabitLand::Fortress,
                         1 | 2 => HabitLand::Town,
                         _ => HabitLand::Village,
@@ -106,19 +178,14 @@ impl Tile {
             }
             ..=4 => {
                 // Mountains and mineis
-                this = if fastrand::u32(..) % 10 == 0 {
+                this = if fastrand::u32(..10) == 0 {
                     Tile::Mine(Default::default())
                 } else {
                     Tile::Mountain
                 };
             }
             _ => {
-                let x = 1 + fastrand::u32(..) % (crate::MAX_PLAYERS as u32 - 1);
-                if x < crate::MAX_PLAYERS as u32 {
-                    this.set_owner(x.into())
-                } else {
-                    this.set_owner(Default::default())
-                }
+                this.set_owner(Player(fastrand::u32(..crate::MAX_PLAYERS as u32)));
             }
         }
 
@@ -129,13 +196,26 @@ impl Tile {
                 ..
             } = this
             {
-                units[u32::from(owner) as usize] = 10;
+                units[owner.0 as usize] = 10;
             } else {
                 unreachable!()
             }
         }
 
         this
+    }
+
+    pub fn set_habitable(&mut self, land: HabitLand) {
+        let l = land;
+        if let Self::Habitable { land, .. } = self {
+            *land = l
+        } else {
+            *self = Self::Habitable {
+                land: l,
+                units: [0; MAX_PLAYERS],
+                owner: Player::NEUTRAL,
+            }
+        }
     }
 }
 
@@ -186,20 +266,104 @@ impl Tile {
 }
 
 /// Shape of the map.
-#[derive(PartialEq, Eq, Clone, Copy, Debug, Hash)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug, Hash, Default)]
 pub enum Stencil {
-    Rect,
     Rhombus,
+    #[default]
+    Rect,
     Hex,
 }
 
 impl Stencil {
     /// Max count of nations of this stencil.
-    #[inline]
-    pub fn max_nations(self) -> usize {
+    pub const fn max_locs(self) -> usize {
         match self {
             Stencil::Rect | Stencil::Rhombus => 4,
             Stencil::Hex => 6,
+        }
+    }
+
+    /// Applies thie stencil to the given grid and
+    /// nation locations slice.
+    pub fn apply(self, grid: &mut Grid, d: u32, locs: &mut [Loc]) {
+        macro_rules! ij {
+            (x, $i:expr, $j:expr) => {
+                0.5 * ($j as f32) + ($i as f32)
+            };
+            (y, $i:expr, $j:expr) => {
+                $j as f32
+            };
+            ($i:expr, $j:expr) => {
+                (ij!(x, $i, $j), ij!(y, $i, $j))
+            };
+        }
+
+        match self {
+            Stencil::Rhombus => {
+                const LOC_NUM: usize = 4;
+                let xs: [_; LOC_NUM] = [d, grid.width - 1 - d, d, grid.width - 1 - d];
+                let ys: [_; LOC_NUM] = [d, grid.height - 1 - d, d, grid.height - 1 - d];
+                for (loc, xy) in locs.iter_mut().zip(xs.into_iter().zip(ys.into_iter())) {
+                    *loc = xy.into();
+                }
+            }
+            Stencil::Rect => {
+                const EPSILON: f32 = 0.1;
+                let (x0, y0) = (ij!(x, 0, grid.height - 1) - EPSILON, ij!(y, 0, 0) - EPSILON);
+                let (x1, y1) = (
+                    ij!(x, grid.width - 1, 0) + EPSILON,
+                    ij!(y, 0, grid.height - 1) + EPSILON,
+                );
+
+                for (i, arr) in grid.tiles.iter_mut().enumerate() {
+                    for (j, tile) in arr.iter_mut().enumerate() {
+                        let (x, y) = ij!(i, j);
+                        if x < x0 || x > x1 || y < y0 || y > y1 {
+                            *tile = Tile::Void;
+                        }
+                    }
+                }
+
+                const LOC_NUM: usize = 4;
+                let dx = grid.height / 2;
+                locs[..LOC_NUM].copy_from_slice(
+                    &[
+                        (dx + d - 1, d),
+                        (grid.width - dx - 1 - d + 1, grid.height - 1 - d),
+                        (d + 1, grid.height - 1 - d),
+                        (grid.width - 1 - d - 1, d),
+                    ]
+                    .map(Loc::from),
+                )
+            }
+            Stencil::Hex => {
+                let dx = grid.height / 2;
+                for (i, arr) in grid
+                    .tiles
+                    .iter_mut()
+                    .enumerate()
+                    .map(|(a, b)| (a as u32, b))
+                {
+                    for (j, tile) in arr.iter_mut().enumerate().map(|(a, b)| (a as u32, b)) {
+                        if i + j < dx || i + j > grid.width - 1 + grid.height - 1 - dx {
+                            *tile = Tile::Void;
+                        }
+                    }
+                }
+
+                const LOC_NUM: usize = 6;
+                locs[..LOC_NUM].copy_from_slice(
+                    &[
+                        (dx + d - 2, d),
+                        (d, grid.height - 1 - d),
+                        (grid.width - 1 - d, dx),
+                        (d, dx),
+                        (grid.width - 1 - d - 2 + 2, d),
+                        (grid.width - 1 - dx - d + 2, grid.height - 1 - d),
+                    ]
+                    .map(Loc::from),
+                )
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+macro_rules! in_segment {
+    ($x:expr, $l:expr, $r:expr) => {
+        if $x < $l {
+            $l
+        } else if $x > $r {
+            $r
+        } else {
+            $x
+        }
+    };
+}
+
 mod grid;
 mod state;
 
@@ -7,30 +19,14 @@ const MAX_HEIGHT: u32 = 29;
 const MAX_PLAYERS: usize = 8;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Default)]
-enum Player {
-    #[default]
-    Neutral,
-    Id(u32),
-}
+struct Player(u32);
 
-impl From<Player> for u32 {
-    #[inline]
-    fn from(value: Player) -> Self {
-        match value {
-            Player::Neutral => 0,
-            Player::Id(id) => id,
-        }
-    }
-}
+impl Player {
+    const NEUTRAL: Self = Self(0);
 
-impl From<u32> for Player {
     #[inline]
-    fn from(value: u32) -> Self {
-        if value == 0 {
-            Self::Neutral
-        } else {
-            Self::Id(value)
-        }
+    fn is_neutral(self) -> bool {
+        self == Self::NEUTRAL
     }
 }
 
