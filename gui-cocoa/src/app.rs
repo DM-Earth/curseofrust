54b1ba6a12c4843ba9935cb4ab4758716a16bd3a
C191239 <zsz2007314zsz@sina.com>
2023-12-18 08:21:03 +0800

    unconscious last night

--- gui-cocoa/src/app.rs
+++ gui-cocoa/src/app.rs
@@ -50,23 +50,25 @@ impl CorApp {
         let string: NSString = NSString::new(name);
         unsafe {
             use cacao::core_graphics::geometry::{CGPoint, CGRect, CGSize};
-            use cacao::foundation::{id, NSMutableDictionary};
+            use cacao::foundation::{id, nil, NSMutableDictionary};
             use cacao::objc::{class, msg_send, sel, sel_impl};
 
             let shared_app: id = msg_send![class!(RSTApplication), sharedApplication];
             let main_menu: id = msg_send![shared_app, mainMenu];
-            let item_zero: id = msg_send![main_menu,itemAtIndex:0];
+            let item_zero: id = msg_send![main_menu, itemAtIndex:0];
             let app_menu: id = msg_send![item_zero, submenu];
 
-            let font: id = msg_send![class!(NSFont),boldSystemFontOfSize:13];
+            let font: id = msg_send![class!(NSFont), boldSystemFontOfSize:13];
             let mut dict: NSMutableDictionary = NSMutableDictionary::new();
             dict.insert(NSString::new("NSFontAttributeName"), font);
-            let size: CGSize = msg_send![string, dict];
+            let dict_objc: id = dict.into_inner();
+            let size: CGSize = msg_send![string, sizeWithAttributes:dict_objc];
             let mut image: id = msg_send![class!(NSImage), alloc];
-            image = msg_send![image,initWithSize:size];
+            image = msg_send![image, initWithSize:size];
             let _: () = msg_send![image, lockFocus];
             let rect: CGRect = CGRect::new(&CGPoint::new(0.0, 0.5), &size);
-            let _: () = msg_send![image,drawWithRect:rect options:1<<0 attributes:dict];
+            let _: () =
+                msg_send![string, drawWithRect:rect options:1<<0 attributes:dict_objc context:nil];
             let _: () = msg_send![image, unlockFocus];
 
             let _: () = msg_send![app_menu, setTitle:NSString::new("")];
