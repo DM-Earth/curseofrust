40caff1e2f1ead951a7bea1f320208306a2ae8ff
JieningYu <jiening.yu@outlook.com>
2024-04-18 22:45:20 +0800

    cocoa gui only for macos

--- gui-cocoa/src/main.rs
+++ gui-cocoa/src/main.rs
@@ -1,8 +1,8 @@
-#[cfg(target_os = "macos")]
+#![cfg(target_os = "macos")]
+
 mod app;
 mod util;
 
 fn main() {
-    #[cfg(target_os = "macos")]
     cacao::appkit::App::new("com.dm.earth.curseofrust", app::CorApp::new()).run();
 }
