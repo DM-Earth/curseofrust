1b5cfde66e3a789dfe5c0bf0676b0f265ce027a4
C191239 <zsz2007314zsz@sina.com>
2024-06-25 11:45:06 +0800

    more

--- ffi/include/curseofrust.h
+++ ffi/include/curseofrust.h
@@ -1,19 +1,35 @@
 #ifndef CURSEOFRUST_H
 #define CURSEOFRUST_H
 
+#include <cstdint>
+#include <stdint.h>
+
 typedef struct {
   void *first;
   void *second;
-} CORParseOptionsReturn;
+} CORFunctionReturn;
+typedef struct {
+  int32_t x;
+  int32_t y;
+} CORPosition;
+typedef struct {
+  CORPosition cursor;
+  uint16_t xskip;
+  uint16_t xlen;
+} CORInterface;
 
 typedef void *CORBasicOptsRef;
 typedef void *CORMultiplayerOptsRef;
 typedef void *CORStateRef;
-typedef void *CORUIRef;
 
-CORParseOptionsReturn CORParseOptions(char *optStringPtr);
-void CORReleaseErrorString(char *errorStringPtr);
-void *CORMakeState(CORBasicOptsRef basicOptsPtr);
-CORUIRef CORMakeUI(CORStateRef statePtr);
+CORFunctionReturn CORParseOptions(char *_Nonnull optStringPtr);
+void CORReleaseErrorString(char *_Nonnull errorStringPtr);
+CORFunctionReturn CORMakeState(CORBasicOptsRef _Nonnull basicOptsPtr);
+CORInterface CORMakeInterface(CORStateRef _Nonnull statePtr);
+uint64_t CORGetSeed(CORStateRef _Nonnull statePtr);
+uint32_t CORGetGridHeight(CORStateRef _Nonnull statePtr);
+uint32_t CORGetGridWidth(CORStateRef _Nonnull statePtr);
+void CORKingsMove(CORStateRef _Nonnull statePtr);
+void CORSimulate(CORStateRef _Nonnull statePtr);
 
 #endif
